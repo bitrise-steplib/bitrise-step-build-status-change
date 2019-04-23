82d0b004297d15378d308430fb16c8ee6d95b1ec
Papik Tamas <trapacska@gmail.com>
2019-04-23 16:09:42 +0200

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# bitrise-step-build-status-change
+# Build Status Change
+
+Exports environment variables to be able to detect if the currently running build's status has changed to a previous one.
+
+
+## How to use this Step
+
+Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
+just `git clone` this repository, `cd` into it's folder in your Terminal/Command Line
+and call `bitrise run test`.
+
+*Check the `bitrise.yml` file for required inputs which have to be
+added to your `.bitrise.secrets.yml` file!*
+
+Step by step:
+
+1. Open up your Terminal / Command Line
+2. `git clone` the repository
+3. `cd` into the directory of the step (the one you just `git clone`d)
+5. Create a `.bitrise.secrets.yml` file in the same directory of `bitrise.yml`
+   (the `.bitrise.secrets.yml` is a git ignored file, you can store your secrets in it)
+6. Check the `bitrise.yml` file for any secret you should set in `.bitrise.secrets.yml`
+  * Best practice is to mark these options with something like `# define these in your .bitrise.secrets.yml`, in the `app:envs` section.
+7. Once you have all the required secret parameters in your `.bitrise.secrets.yml` you can just run this step with the [bitrise CLI](https://github.com/bitrise-io/bitrise): `bitrise run test`
+
+An example `.bitrise.secrets.yml` file:
+
+```
+envs:
+- A_SECRET_PARAM_ONE: the value for secret one
+- A_SECRET_PARAM_TWO: the value for secret two
+```
---
+++ main.go
@@ -0,0 +1,242 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-steputils/tools"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/google/go-querystring/query"
+	"github.com/pkg/errors"
+)
+
+const (
+	baseURL                   = "https://api.bitrise.io/v0.1"
+	buildTypeTag              = "tag"
+	buildTypePush             = "push"
+	buildTypeManual           = "manual"
+	buildTypePullRequest      = "pull-request"
+	envKeyPreviousBuildStatus = "PREVIOUS_BUILD_STATUS"
+	envKeyBuildStatusChanged  = "BUILD_STATUS_CHANGED"
+	statusTextSuccessfulBuild = "success"
+)
+
+type config struct {
+	AppSlug             string          `env:"BITRISE_APP_SLUG,required"`
+	BuildSlug           string          `env:"BITRISE_BUILD_SLUG,required"`
+	BuildStatus         string          `env:"BITRISE_BUILD_STATUS,required"`
+	PreviousBuildStatus string          `env:"PREVIOUS_BUILD_STATUS"`
+	AccessToken         stepconf.Secret `env:"access_token,required"`
+}
+
+func (cfg config) getBuild() (build, error) {
+	url := baseURL + "/apps/" + cfg.AppSlug + "/builds/" + cfg.BuildSlug
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return build{}, err
+	}
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", string(cfg.AccessToken))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return build{}, err
+	}
+	var b struct{ Data build }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return build{}, err
+	}
+	if err := json.Unmarshal(body, &b); err != nil {
+		return build{}, errors.Wrap(err, string(body))
+	}
+	return b.Data, nil
+}
+
+func (cfg config) getBuilds(f filter) (builds, error) {
+	url := baseURL + "/apps/" + cfg.AppSlug + "/builds"
+
+	f.SortBy = "created_at"
+	queryParams, err := query.Values(f)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.RawQuery = queryParams.Encode()
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", string(cfg.AccessToken))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	var builds struct {
+		Data []build `json:"data"`
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(body, &builds); err != nil {
+		return nil, errors.Wrap(err, string(body))
+	}
+	return builds.Data, nil
+}
+
+type filter struct {
+	Before           int    `url:"before,omitempty"`
+	Branch           string `url:"branch,omitempty"`
+	SortBy           string `url:"sort_by,omitempty"`
+	Workflow         string `url:"workflow,omitempty"`
+	PullRequestID    int    `url:"pull_request_id,omitempty"`
+	TriggerEventType string `url:"trigger_event_type,omitempty"`
+}
+
+func (f filter) String() string {
+	return fmt.Sprintf("- branch: %s\n- workflow: %s\n- pull request ID: %d\n- event type: %s", f.Branch, f.Workflow, f.PullRequestID, f.TriggerEventType)
+}
+
+type build struct {
+	Tag                     string    `json:"tag"`
+	Slug                    string    `json:"slug"`
+	Branch                  string    `json:"branch"`
+	Status                  int       `json:"status"`
+	CommitHash              string    `json:"commit_hash"`
+	StatusText              string    `json:"status_text"`
+	TriggeredAt             time.Time `json:"triggered_at"`
+	BuildNumber             int64     `json:"build_number"`
+	PullRequestID           *int64    `json:"pull_request_id"`
+	TriggeredWorkflow       string    `json:"triggered_workflow"`
+	PullRequestTargetBranch string    `json:"pull_request_target_branch"`
+}
+
+func (build build) generateFilter() filter {
+	f := filter{
+		Workflow: build.TriggeredWorkflow,
+		Branch:   build.Branch,
+		Before:   int(build.TriggeredAt.Unix()),
+	}
+	switch build.buildType() {
+	case buildTypeTag:
+		f.TriggerEventType = "tag"
+	case buildTypePullRequest:
+		f.TriggerEventType = "pull-request"
+		f.PullRequestID = int(*build.PullRequestID)
+	default: // website handles manual as push
+		f.TriggerEventType = "push"
+	}
+	return f
+}
+
+func (build build) buildType() string {
+	switch {
+	case build.Tag != "":
+		return buildTypeTag
+	case *build.PullRequestID > 0 || (build.PullRequestTargetBranch != ""):
+		return buildTypePullRequest
+	case build.CommitHash == "":
+		return buildTypeManual
+	default:
+		return buildTypePush
+	}
+}
+
+func (build build) equivalent(pair build) bool {
+	return build.Branch == pair.Branch &&
+		build.TriggeredWorkflow == pair.TriggeredWorkflow &&
+		build.buildType() == pair.buildType()
+}
+
+type builds []build
+
+func (builds builds) previous(actualBuild build) (build, error) {
+	for _, build := range builds {
+		// skip if found the current build or any in-progress ones
+		if build.BuildNumber == actualBuild.BuildNumber || build.Status == 0 {
+			continue
+		}
+		if build.equivalent(actualBuild) {
+			return build, nil
+		}
+	}
+	return build{}, fmt.Errorf("no equivalent build found")
+}
+
+func failf(f string, args ...interface{}) {
+	log.Errorf(f, args...)
+	os.Exit(1)
+}
+
+func main() {
+	var cfg config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
+	}
+	stepconf.Print(cfg)
+	fmt.Println()
+
+	if cfg.PreviousBuildStatus != "" {
+		log.Infof("Exporting environment variables:")
+		currentBuildFailed, previousBuildFailed := cfg.BuildStatus != "0", cfg.PreviousBuildStatus != statusTextSuccessfulBuild
+		changed := fmt.Sprintf("%t", currentBuildFailed != previousBuildFailed)
+		if err := tools.ExportEnvironmentWithEnvman(envKeyBuildStatusChanged, changed); err != nil {
+			failf("failed to export env: %s, error: %s", envKeyBuildStatusChanged, err)
+		}
+		log.Printf("- %s=%s", envKeyBuildStatusChanged, changed)
+		log.Donef("- Done")
+		return
+	}
+
+	log.Infof("Getting current build")
+	currentBuild, err := cfg.getBuild()
+	if err != nil {
+		failf("- Failed to get current build, error: %s", err)
+	}
+	log.Printf("Build info:")
+	filter := currentBuild.generateFilter()
+	log.Printf("%s", filter)
+	log.Donef("- Done")
+	fmt.Println()
+
+	log.Infof("Getting similar builds")
+	similarBuilds, err := cfg.getBuilds(filter)
+	if err != nil {
+		failf("- Failed to get similar builds, error: %s", err)
+	}
+	log.Printf("%d builds found", len(similarBuilds))
+	log.Donef("- Done")
+	fmt.Println()
+
+	log.Infof("Looking for previous build")
+	matchingPreviousBuild, err := similarBuilds.previous(currentBuild)
+	if err != nil {
+		failf("- Failed to find previous build, error: %s", err)
+	}
+	log.Donef("- Found: (#%d) %s: %s", matchingPreviousBuild.BuildNumber, matchingPreviousBuild.Slug, matchingPreviousBuild.StatusText)
+	fmt.Println()
+
+	previousBuildFailed := matchingPreviousBuild.StatusText != statusTextSuccessfulBuild
+
+	log.Infof("Exporting environment variables:")
+	if err := tools.ExportEnvironmentWithEnvman(envKeyPreviousBuildStatus, matchingPreviousBuild.StatusText); err != nil {
+		failf("failed to export env: %s, error: %s", envKeyPreviousBuildStatus, err)
+	}
+	log.Printf("- %s=%s", envKeyPreviousBuildStatus, matchingPreviousBuild.StatusText)
+
+	currentBuildFailed := cfg.BuildStatus != "0"
+	changed := fmt.Sprintf("%t", currentBuildFailed != previousBuildFailed)
+	if err := tools.ExportEnvironmentWithEnvman(envKeyBuildStatusChanged, changed); err != nil {
+		failf("failed to export env: %s, error: %s", envKeyBuildStatusChanged, err)
+	}
+	log.Printf("- %s=%s", envKeyBuildStatusChanged, changed)
+	log.Donef("- Done")
+}
